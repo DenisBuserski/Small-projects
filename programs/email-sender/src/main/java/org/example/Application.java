6c6d590d75a429738ab8f810780b05c53f2036d7
DenisKP <denis.buserski@gmail.com>
2023-11-07 21:55:39 +0200

    Update

--- programs/email-sender/src/main/java/org/example/Application.java
+++ programs/email-sender/src/main/java/org/example/Application.java
@@ -1,35 +1,43 @@
 package org.example;
 
+import org.example.logmanipulator.Log1;
+import org.example.logmanipulator.Log2;
+import org.example.logmanipulator.LogManipulator;
+
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 import java.util.Scanner;
 
 public class Application {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
+        List<String> result = new ArrayList<>();
 
-        File folder = new File("src/main/resources");
-        File[] listOfFiles = folder.listFiles();
-        String result = "";
-
-        for (File file : listOfFiles) {
-            int count = 0;
-            Scanner scanner = new Scanner(file);
-            while (scanner.hasNext()) {
-                String line = scanner.nextLine();
-                if (line.equals("Denis")) {
-                    count++;
-                }
-            }
-            result = "Denis = " + count;
+        Scanner scanner = new Scanner(System.in);
+        String task = scanner.nextLine();
+        switch (task) {
+            case "1":
+                List<String> stringFile1 = FileReader.readFile("src/main/resources/logs-1.txt");
+                LogManipulator log1Manipulator = new Log1();
+                result = log1Manipulator.manipulate(stringFile1);
+                break;
+            case "2":
+                List<String> stringFile2 = FileReader.readFile("src/main/resources/logs-2.txt");
+                LogManipulator log1Manipulator2 = new Log2();
+                result = log1Manipulator2.manipulate(stringFile2);
+                break;
+            default:
+                throw new FileNotFoundException("No files found!");
         }
-
+        
         // Outlook email configuration
         String smtpHost = "outlook.office365.com";
         String sender = "java_test_d@outlook.com";
@@ -55,8 +63,14 @@ public class Application {
             message.setFrom(internetAddressSender);
             InternetAddress internetAddressReceiver = new InternetAddress(recipient);
             message.setRecipient(Message.RecipientType.TO, internetAddressReceiver);
-            message.setSubject("Hello, this is a test email");
-            message.setText("This is a test email sent from Java. I searched for Denis in logs.txt and I found that " + result);
+            message.setSubject("Invalid logins");
+
+            StringBuilder msg = new StringBuilder();
+            msg.append("We have found ").append(result.size()).append(" logins. Please find below the usernames that were not able to login:").append("\n");
+            for (String s : result) {
+                msg.append(s).append("\n");
+            }
+            message.setText(msg.toString());
 
             // Connect and authenticate recipient the SMTP server
             Transport transport = session.getTransport("smtp");
---
+++ programs/email-sender/src/main/java/org/example/logmanipulator/Log2.java
@@ -0,0 +1,10 @@
+package org.example.logmanipulator;
+
+import java.util.List;
+
+public class Log2 implements LogManipulator {
+    @Override
+    public List<String> manipulate(List<String> stringFile) {
+        return null;
+    }
+}
