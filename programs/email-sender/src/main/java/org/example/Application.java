9c16b8cb9b4a1c3a5bf7c53813d87daf17a3f2e2
DenisKP <denis.buserski@gmail.com>
2023-11-07 21:58:37 +0200

    Update

--- programs/email-sender/src/main/java/org/example/Application.java
+++ programs/email-sender/src/main/java/org/example/Application.java
@@ -40,8 +40,8 @@ public class Application {
         
         // Outlook email configuration
         String smtpHost = "outlook.office365.com";
-        String sender = "test@outlook.com";
-        String password = "**********";
+        String sender = "java_test_d@outlook.com";
+        String password = "123456789Test";
 
         // Recipient's email address
         String recipient = "denis.buserski@gmail.com";
