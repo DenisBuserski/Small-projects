4bb392bc3c98f27ed8c889d61d91eb83ec452437
DenisKP <denis.buserski@gmail.com>
2023-11-08 20:32:47 +0200

    Update

--- programs/email-sender/src/main/java/org/example/exceptions/MessageNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.example.exceptions;
-
-public class MessageNotFoundException extends RuntimeException {
-    public MessageNotFoundException(String reason) {
-        super(reason);
-    }
-}
--- programs/email-sender/src/main/java/org/example/logmanipulator/LogManipulator.java
+++
@@ -1,7 +0,0 @@
-package org.example.logmanipulator;
-
-import java.util.List;
-
-public interface LogManipulator {
-    List<String> manipulate(List<String> stringFile);
-}
